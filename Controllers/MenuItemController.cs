b83266c4a70844c32bc771af28ed982b97e4e59f
Asus <Asus@LAPTOP-6BD0C3OE>
2021-09-23 17:09:34 +0530

    Add project files.

---
+++ Controllers/MenuItemController.cs
@@ -0,0 +1,43 @@
+﻿using MenuItemListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuItemListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        static List<MenuItem> items = new List<MenuItem>()
+        {
+            new MenuItem(){Id=1,Name="Vikash1",freeDelivery=true,Price=200,dateOfLaunch=new DateTime(2021,9,21),Active=true},
+            new MenuItem(){Id=2,Name="Vikash2",freeDelivery=false,Price=500.6,dateOfLaunch=new DateTime(2021,9,21),Active=true},
+            new MenuItem(){Id=3,Name="Vikash3",freeDelivery=true,Price=700,dateOfLaunch=new DateTime(2021,9,21),Active=false}
+        };
+
+        [HttpGet]
+        public IActionResult GetItem()
+        {
+            return Ok(items);
+        }
+
+        [HttpGet]
+        public IActionResult GetItem(int id)
+        {
+            foreach (var i in items)
+            {
+                if (i.Id == id)
+                {
+                    return Ok(i.Name);
+                }
+            }
+            return NotFound("Invalid Id");
+        }
+
+    }
+}
---
+++ Models/MenuItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuItemListing.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Free Delivery")]
+        public bool freeDelivery { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime dateOfLaunch { get; set; }
+        //for cheking active
+        //checking for my commits  ndbd njnj
+        public bool isInactive { get; set; }  
+
+        public bool Active { get; set; }
+    }
+}
